e1c916c6c1838bcaee686c1d8267016c4502bbe6
Flu3nt <gnedyalkov94@gmail.com>
2023-06-20 09:35:28 +0300

    feature: add a maze solver solution and test

--- data-structures/recursion.js
+++ data-structures/recursion.js
@@ -3,18 +3,105 @@ function recusiveSum(n) {
   if (n === 1) {
     return 1;
   }
-
   // pre recursion
   let out = n + recusiveSum(n - 1);
-
   // post recursion
   //   console.log(out);
-
   return out;
 }
 
-recusiveSum(10);
+const dir = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
+
+function walk(maze, wall, curr, end, seen, path) {
+  // 1. base cases:
+  // off the map
+  if (
+    curr.x < 0 ||
+    curr.x >= maze[0].length ||
+    curr.y < 0 ||
+    curr.y >= maze.length
+  ) {
+    return false;
+  }
+
+  // its a wall
+  if (maze[curr.y][curr.x] === wall) {
+    return false;
+  }
+
+  // are we at the end
+  if (curr.x === end.x && curr.y === end.y) {
+    path.push(end);
+    return true;
+  }
+
+  // we have seen it
+  if (seen[curr.y][curr.x]) {
+    return false;
+  }
+
+  // 3 recurse
+  // pre
+  seen[curr.y][curr.x] = true;
+  path.push(curr);
+  // recurse
+  for (let i = 0; i < dir.length; ++i) {
+    // we are going in a direction and need to keep a track of our path.
+    const [x, y] = dir[i];
+    if (
+      walk(
+        maze,
+        wall,
+        {
+          x: curr.x + x,
+          y: curr.y + y,
+        },
+        end,
+        seen,
+        path
+      )
+    ) {
+      return true;
+    }
+  }
+
+  // post
+  path.pop();
+
+  return false;
+}
+
+function solve(maze, wall, start, end) {
+  const seen = [];
+  const path = [];
+
+  // create the maze to check
+  for (let i = 0; i < maze.length; ++i) {
+    seen.push(new Array(maze[0].length).fill(false));
+  }
+
+  walk(maze, wall, start, end, seen, path);
+
+  return path;
+}
+
+const maze = [
+  "XXXXXXXXXX X",
+  "X        X X",
+  "X        X X",
+  "X XXXXXXXX X",
+  "X          X",
+  "X XXXXXXXXXX",
+];
+
+solve(maze, "X", { x: 10, y: 0 }, { x: 1, y: 5 });
 
 module.exports = {
   recusiveSum,
+  solve,
 };
--- data-structures/test/recursion.test.js
+++ data-structures/test/recursion.test.js
@@ -1,9 +1,40 @@
-const { recusiveSum } = require("../recursion");
+const { recusiveSum, solve } = require("../recursion");
 
 describe("recursiveSum", () => {
   it("should sum all numbers together recursively", () => {
     let sum = recusiveSum(10);
 
     expect(sum).toBe(55);
+  });
+});
+
+describe("solve", () => {
+  it("should find the right path of a maze", () => {
+    const maze = [
+      "XXXXXXXXXX X",
+      "X        X X",
+      "X        X X",
+      "X XXXXXXXX X",
+      "X          X",
+      "X XXXXXXXXXX",
+    ];
+    //  solve(maze, wall, start, end)
+    expect(solve(maze, "X", { x: 10, y: 0 }, { x: 1, y: 5 })).toEqual([
+      { x: 10, y: 0 },
+      { x: 10, y: 1 },
+      { x: 10, y: 2 },
+      { x: 10, y: 3 },
+      { x: 10, y: 4 },
+      { x: 9, y: 4 },
+      { x: 8, y: 4 },
+      { x: 7, y: 4 },
+      { x: 6, y: 4 },
+      { x: 5, y: 4 },
+      { x: 4, y: 4 },
+      { x: 3, y: 4 },
+      { x: 2, y: 4 },
+      { x: 1, y: 4 },
+      { x: 1, y: 5 },
+    ]);
   });
 });
