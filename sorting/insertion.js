7209ddec5092877910f4dfe1f76358a0a18e0542
Flu3nt <gnedyalkov94@gmail.com>
2023-09-22 12:04:50 +0300

    implement insertion sort

--- sorting/insertion.js
+++ sorting/insertion.js
@@ -1,5 +1,18 @@
-function insertionSort(arr) {}
+function insertionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let j = i;
 
-const unsortedArr = [2, 1, 90, 34, -2, 9, 290, 100];
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            let temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+
+    return arr;
+}
+
+const unsortedArr = [2, 8, 5, 3, 9, 4];
 
 console.log(insertionSort(unsortedArr));
--- sorting/practice.js
+++ sorting/practice.js
@@ -1,3 +1,11 @@
+function swap(arr, firstIndex, secondIndex) {
+    let temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+
+    return arr;
+}
+
 function bubbleSort(arr) {
     // iterate through all elements
     for (let i = 0; i < arr.length; i++) {
@@ -5,12 +13,47 @@ function bubbleSort(arr) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             // if the first element is greater than the second element, swap the elements.
             if (arr[j] > arr[j + 1]) {
-                let temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                swap(arr, j, j + 1);
             }
         }
     }
     // return the array
     return arr;
 }
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (minIndex !== i) {
+            swap(arr, minIndex, i);
+        }
+    }
+
+    return arr;
+}
+
+function insertionSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let j = i;
+
+        while (j > 0 && arr[j - 1] > arr[j]) {
+            swap(arr, j - 1, j);
+            j--;
+        }
+    }
+
+    return arr;
+}
+
+const unsortedArr = [9, 4, 8, 7, 2, 0, 1, 5];
+
+const sortedArr = insertionSort(unsortedArr);
+
+console.log(sortedArr);
